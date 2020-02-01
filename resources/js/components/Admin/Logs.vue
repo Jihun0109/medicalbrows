682d3be5d32cef03573a321846aa6aa4e79e1e8d
MingChuan Jiang <trueshinygold@gmail.com>
2020-02-01 10:52:05 +0800

    modify log message

--- resources/js/components/Admin/Logs.vue
+++ resources/js/components/Admin/Logs.vue
@@ -6,7 +6,7 @@
                     <div class="card-header">Example Component</div>
 
                     <div class="card-body">
-                        I'm an Logs component.
+                        I'm an Logs component. Welcome...
                     </div>
                 </div>
             </div>
@@ -15,9 +15,9 @@
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
+export default {
+    mounted() {
+        console.log("Component mounted.");
     }
+};
 </script>
